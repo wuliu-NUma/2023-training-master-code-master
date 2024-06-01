92e3bc52de5bc1d6e23bfabc8d5237e0d48bba86
HOLO4 <2404063434@qq.com>
2024-06-01 20:39:11 +0800

    创建 text.py

---
+++ python颜色识别/text.py
@@ -0,0 +1,15 @@
+# Untitled - By: 10416 - 周六 7月 8 2023
+
+import sensor, image, time
+
+sensor.reset()
+sensor.set_pixformat(sensor.RGB565)
+sensor.set_framesize(sensor.QVGA)
+sensor.skip_frames(time = 2000)
+
+clock = time.clock()
+
+while(True):
+    clock.tick()
+    img = sensor.snapshot()
+    print(clock.fps())
